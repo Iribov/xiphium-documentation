edd6ecf53958316fdaf59d0a0663e5a7282f2b88
Jim Kerver <jim.kerver@iribov.com>
2024-08-05 18:12:21 -0600

    Change social card to Iribov Logo
    Might need to become a .png instead of .svg.
    But let's wait for the new logo before adjusting these kinds of things for now.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
       ],
 
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/logo.svg',
       navbar: {
         title: 'Xiphium',
         logo: {
